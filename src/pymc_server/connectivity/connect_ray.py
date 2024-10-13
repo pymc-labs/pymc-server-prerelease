11b6913e8b113be60d2f51f22b886a8b8725178e
Chris <chris@myzel.io>
2024-10-13 17:29:36 +0200

    adding local attributes to the wrapper class
    previously we've been unable to do `self.foo` local attributes on the
    wrapper.
    
    An allowlist has been added to decide which attributes are treated
    locally and which we try to resolve on the remote actor

--- src/pymc_server/connectivity/connect_ray.py
+++ src/pymc_server/connectivity/connect_ray.py
@@ -1,5 +1,12 @@
 import ray
 from ray import remote
+from pymc_server.config.config_connectivity import (
+    DEFAULTS_CONNECTIVITY_YOU_ARE_NOT_CONNECTED_MSG,
+    DEFAULTS_STATEFUL_ACTOR_NAME,
+)
+
+class NotConnectedError(Exception):
+    pass
 
 @remote
 class Actor:
@@ -38,20 +45,53 @@ class RemoteStatefulActorWrapper:
             *args, 
             **kwargs
     ):
-        if allow_reconnect:
-            self.actor = ray.get_actor('remote_stateful_actor', namespace=namespace)
-        else:
-            self.actor = implementation.options(
-                name="remote_stateful_actor",
-                lifetime="detached", # this should never chagne
-                namespace=namespace
-            ).remote(*args, **kwargs)
+        # List of local attributes that are resolved NOT remotely, we have overwritten __setattr__ and __getattr__ so we can't just do self.foo
+        self.__dict__['local_attrs'] = set([
+            'foo',
+            'bar',
+            'local_attrs',
+            'allow_reconnect',
+            'is_connected',
+            'actor',
+        ]) 
+        # we can only do this with attributes named above
+        self.is_connected = False
+        self.allow_reconnect = allow_reconnect
+        if self.allow_reconnect:
+            try: 
+                print('trying to reconnect')
+                self.actor = ray.get_actor(
+                    'remote_stateful_actor',
+                    namespace=namespace
+                )
+                self.is_connected = True
+            except ValueError as e:
+                print(e)
+                self._raise_not_connected()
 
+        if not(self.is_connected):
+            try: 
+                self.actor = implementation.options(
+                    name="remote_stateful_actor",
+                    lifetime="detached", # this should never chagne
+                    namespace=namespace
+                ).remote(*args, **kwargs)
 
+            except ValueError as e:
+                print(e)
+                self._raise_not_connected()
+
+    def _raise_not_connected(self, custom_message=None):
+        msg = custom_message if custom_message else DEFAULTS_CONNECTIVITY_YOU_ARE_NOT_CONNECTED_MSG 
+        raise NotConnectedError(msg)
     def __kill__(self):
         ray.kill(self.actor)
 
     def __getattr__(self, attr_name):
+        local_attrs = super().__getattribute__('local_attrs')
+        if attr_name in local_attrs:
+            return self.__dict__[attr_name] 
+
         attr= ray.get(self.actor.get_attribute.remote(attr_name))
 
         # If the attribute is a method, return a function that calls the method remotely
@@ -66,10 +106,11 @@ class RemoteStatefulActorWrapper:
             return ray.get(self.actor.get_attribute.remote(attr_name))
 
     def __setattr__(self, attr_name, value):
-        print(attr_name)
-        if attr_name == "actor":
+        if attr_name in self.__dict__['local_attrs']:
+            # If the attribute is a local attribute, set it locally
             self.__dict__[attr_name] = value
         else:
+            # If the attribute is not a local attribute, set it on the actor
             self.actor.set_attribute.remote(attr_name, value)
 
 

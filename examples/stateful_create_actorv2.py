1a4c3947760feaa2e88803c1488af1ca7391953c
Chris <chris@myzel.io>
2024-10-13 16:18:09 +0200

    fully working

--- examples/stateful_create_actorv2.py
+++ examples/stateful_create_actorv2.py
@@ -3,87 +3,6 @@
 
 import ray
 
-"""
-@ray.remote
-class StatefulActor:
-    def __init__(self):
-        super().__setattr__("state", {})  # Initialize state
-
-    def __setattr__(self, key, value):
-        print('key: '+ key + ', value:', value)
-        if key.startswith('_') or key == 'state':
-            super().__setattr__(key, value)
-        else:
-            # Set the value using remote call
-            ray.get(self.set_value.remote(key, value))
-
-    def __getattr__(self, key):
-        print(key)
-        # Dynamically fetch the value from the actor's state
-        if key.startswith('_') or key == 'state':
-            raise AttributeError(f"Attribute '{key}' not found")  # Avoid infinite loop
-
-        return ray.get(self.get_value.remote(key))
-
-    def foobar(self):
-        self.invocation_count +=1  
-        print("_______FOOOBAR________")
-        print(self.invocation_count)
-        #self.set_value('invocation_count', self.invocation_count + 1)
-        return "foobar"
-
-    def set_value(self, key, value):
-        self.state[key] = value
-
-    def get_value(self, key):
-        return self.state.get(key, None)
-
-"""
-import ray
-
-@ray.remote
-class StatefulActor:
-    def __init__(self):
-        # Initialize internal state attributes
-        self._state = {}  # Use a private variable for dynamic attributes
-        self.invocation_count = 0  # Internal state attribute
-
-    """
-    def __setattr__(self, key, value):
-        print('set', key, value)
-        # Allow normal assignment for internal attributes
-        if '_' in key:
-            # Directly set internal state attributes
-            super().__setattr__(key, value)
-        else:
-            # Store dynamic attributes in the _state dictionary
-            self._state[key] = value
-
-    def __getattr__(self, key):
-        print('get', key)
-        # Allow normal retrieval for internal attributes
-        if '_' in key:
-            return super().__getattribute__(key)
-        # Return from dynamic state
-        return self._state.get(key, None)
-    """
-
-    def get_attribute(self, attr_name):
-        return getattr(self, attr_name)
-
-    def foobar(self):
-        # Increment invocation_count normally
-        print('will halt')
-        breakpoint()
-        self.invocation_count += 1
-        return self.invocation_count
-
-    def get_invocation_count(self):
-        return self.invocation_count
-
-    def get_dynamic_value(self, key):
-        return self._state.get(key, None)
-
 @ray.remote
 class Actor:
     def __init__(self, foo, bar):
@@ -91,6 +10,9 @@ class Actor:
         self.bar = bar
         self.invocation_count = 0
         self.baz = 1
+
+    def reset(self):
+        self.invocation_count = 0
 
     def _foobar(self):
         print(super())
@@ -112,13 +34,13 @@ class ActorWrapper:
 
 
     def __getattr__(self, attr_name):
-        # Get the attribute from the actor
         attr= ray.get(self.actor.get_attribute.remote(attr_name))
 
         # If the attribute is a method, return a function that calls the method remotely
         if callable(attr):
             def func(*args, **kwargs):
-                return ray.get(attr.remote(*args, **kwargs))
+                method = getattr(self.actor, attr_name)
+                return ray.get(method.remote(*args, **kwargs))
             return func
 
         # If the attribute is not a method, get its latest value from the actor
@@ -144,23 +66,6 @@ wrapper = ActorWrapper('foo', 'bar')
 foo = wrapper.foo
 wrapper.foo = 'new_foo_value'
 
-"""
-breakpoint()
-# Create a detached actor under the namespace "my_namespace"
-actor = StatefulActor.options(
-    name="stateful_actor", 
-    lifetime="detached", 
-    namespace="my_namespace"
-).remote()
-
-#  breakpoint()
-# Use the abstracted way to set and get values dynamically
-actor.key1 = 'value1'  # Abstracted assignment
-print(ray.get(actor.key1))  # Outputs: value1
-
-actor.key2 = 'value2'
-print(ray.get(actor.key2))  # Outputs: value2
-
-"""
 ray.shutdown()
+print("make sure to kill the actor with the `examples/stateful_kill_actor.py` script")
 
--- examples/stateful_reconnect_actorv2.py
+++ examples/stateful_reconnect_actorv2.py
@@ -5,8 +5,6 @@ import ray
 
 ray.init(namespace="my_namespace")
 
-#ray.init()
-
 class ActorWrapper:
     def __init__(self, actor_name, namespace):
         self.actor = ray.get_actor(actor_name, namespace=namespace)
@@ -20,7 +18,6 @@ class ActorWrapper:
         if callable(attr):
             def func(*args, **kwargs):
                 method = getattr(self.actor, attr_name)
-                breakpoint()
                 return ray.get(method.remote(*args, **kwargs))
             return func
 
@@ -40,23 +37,5 @@ print(foo)
 wrapper._foobar()
 ic = wrapper.invocation_count
 breakpoint()
-# Get the actor from the namespace "my_namespace"
-"""
-actor = ray.get_actor("stateful_actor")
-remote_val = actor.foobar.remote()
-print(ray.get(remote_val))
-
-remote_val = actor.foobar.remote()
-print(ray.get(remote_val))
-
-#assert  remote_val == 'bar'
-
-# Use the abstracted way to set and get values dynamically
-actor.key1 = 'value1'  # Abstracted assignment
-print(ray.get(actor.key1))  # Outputs: value1
-
-actor.key2 = 'value2'
-print(ray.get(actor.key2))  # Outputs: value2
-"""
 
 ray.shutdown()

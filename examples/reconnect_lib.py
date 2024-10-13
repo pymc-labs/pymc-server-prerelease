b88558bb8f9b7687c3ebd8ceff9d0b3bcc356ae4
Chris <chris@myzel.io>
2024-10-13 23:05:06 +0200

    cleanup for demo

--- examples/reconnect_lib.py
+++ examples/reconnect_lib.py
@@ -1,10 +1,13 @@
-from pymc_server.connectivity.connect_ray import RemoteStatefulActorWrapper, Actor
+from pymc_server.connectivity.connect_ray import(
+    RemoteStatefulActorWrapper,
+    Actor
+)
 
 pymcs_namespace = 'dev_2'
 
 server = RemoteStatefulActorWrapper(
     pymcs_namespace,
-    Actor,
+    implementation=Actor,
     allow_local_fallback=False,
     foo='Pizza Hawaii',
     bar='911'
